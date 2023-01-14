2a46317da112124185366e23d7430b44c35d691d
falko <115454465+FalkoKa@users.noreply.github.com>
2023-01-14 19:39:27 +0700

    Add function for each operator

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a +b;
+}
+
+function subtract(a, b) {
+    return a - b;
+    // a has to be the bigger number
+    // minus * minus = plus...
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+    // round numbers
+    // case for dividing by 0
+}
+
